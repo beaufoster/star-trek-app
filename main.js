061706050313cb3b75e8f11bb048b9d6e4b3e95c
bfoster4 <beaudfoster@gmail.com>
2024-12-16 13:37:36 -0600

    updates

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ async function apiRequest(){
         document.getElementById('alienFeatures').innerText = data.features
         document.getElementById('alienFacts').innerText = data.interestingFact
         document.getElementById('alienExamples').innerText = data.notableExamples
+
         document.getElementById('alienImage').src = data.image
         document.getElementById('alienCaption').innerText = data.speciesName
     }catch(error){
